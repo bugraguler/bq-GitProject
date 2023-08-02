cd5d3a79b68812aab995091c0229e0bed7664dc7
bugraguler <bgrglr.be@gmail.com>
2023-08-02 11:40:01 +0200

    second commit git

--- src/test/java/class04/GitBasics.java
+++ src/test/java/class04/GitBasics.java
@@ -3,5 +3,7 @@ package class04;
 public class GitBasics {
     public static void main(String[] args) {
         System.out.println("Git first print");
+        System.out.println("Git second print");
+        System.out.println("Git first print");
     }
 }

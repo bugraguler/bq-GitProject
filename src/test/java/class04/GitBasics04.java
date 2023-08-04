a124de573c909c17bd32ff2832a00f9cd61fc7e0
bugraguler <bgrglr.be@gmail.com>
2023-08-04 17:50:20 +0200

    lastcommit0408

--- src/test/java/class04/GitBasics04.java
+++ src/test/java/class04/GitBasics04.java
@@ -3,5 +3,8 @@ package class04;
 public class GitBasics04 {
     public static void main(String[] args) {
         System.out.println("dddd");
+
+        //hello ı am working on a project
+
     }
 }
